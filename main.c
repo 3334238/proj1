04037850c6d65631d168cffbffa26630d2ca7e4e
3334238 <48506564+3334238@users.noreply.github.com>
2019-04-03 18:47:31 +1100

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//task 3.2 code
+int main() {
+char message[25], ch; //
+int i, key;
+
+printf("enter a message to encrypt");
+
+printf("Enter key:");
+scanf("%d", &key);
+
+for(i = 0; message[i] !='\0'; ++i){
+    ch = message[i];
+    
+    if(ch >='a' &&ch <= 'z'){
+        ch = ch - key;
+        if(ch < 'a'){
+            ch = ch + 'z' - 'a' + 1;
+        }
+        
+        message [i] = ch;}
+//testiyufguyfghg
+  return 0;
+}
