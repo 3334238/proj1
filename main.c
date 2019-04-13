df3a8529d54fb6eac377efb1f22524d9e0876c98
3334238 <48506564+3334238@users.noreply.github.com>
2019-04-13 11:46:30 +1000

    fixed lower case issue
    unsure if it will operate given the string library and strlwr function

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-char message[100], ch;
+char message[100], ch; //message string
 	int i, key;
 	
 	printf("Enter a message to encrypt: ");
@@ -9,31 +10,33 @@ char message[100], ch;
 	printf("Enter key: ");
 	scanf("%d", &key);
 	
-	for(i = 0; message[i] != '\0'; ++i){
+	for(i = 0; message[i] != '\0'; i++){
 		ch = message[i];
-		if(ch >= 'A' && ch <= 'Z'){
+		if(ch >= 'A' && ch <= 'Z'){ //rotation for upper case letters
 			ch = ch + key;
 			
 			if(ch > 'Z'){
-				ch = ch - 'Z' + 'A' - 1;
+				ch = ch - 'Z' + 'A' - 1; //the actual rotation
 			}
 			
 			message[i] = ch;
+			printf("Encrypted message: %s", message); //prints encrypted message
 		}
 		
-		else if(ch >= 'a' && ch <= 'z'){
+		else if(ch >= 'a' && ch <= 'z'){ //rotation for lower case letters
 			ch = ch + key;
 			
-			if(ch > 'Z'){
-				ch = ch - 'z' + 'a' - 1 - 32; //still having edge problems
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1; // have to rotate from upper case to lower case
 			}
 			
 			message[i] = ch;
+			printf("Encrypted message: %s", strlwr(message)); //strlwr function prints the encrypted message in capital letters
 		}
 		
 	}
 	
-	printf("Encrypted message: %s", message);
+	
 	
 	return 0;
 }         
